e9b218079b2e2b818786a2bb36f600d27867efb3
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-05-17 04:21:39 -0600

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Italo Stuardo on 17/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
